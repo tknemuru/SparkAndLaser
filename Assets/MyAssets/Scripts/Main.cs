b27e99e7623ab8aa23c9f4ef2ad9ac5f7780e49f
tknemuru <kojikoji.max.5252@gmail.com>
2015-04-30 00:25:12 +0900

    rename

--- Assets/MyAssets/Scripts/Main.cs
+++ Assets/MyAssets/Scripts/Main.cs
@@ -3,22 +3,42 @@ using System.Collections;
 
 namespace SparkAndLaser
 {
+    /// <summary>
+    /// ゲームメインクラス
+    /// </summary>
     public class Main : MonoBehaviour
     {
-
+        /// <summary>
+        /// スタートボタン
+        /// </summary>
         public GameObject GameStartButton;
-        public GameObject StarFighter;
+
+        /// <summary>
+        /// ゲームオーバーボタン
+        /// </summary>
         public GameObject GameOverButton;
 
+        /// <summary>
+        /// 自機
+        /// </summary>
+        public GameObject Fighter;
+
+        /// <summary>
+        /// ゲームオーバーかどうか
+        /// </summary>
         public bool IsGameOver { get; set; }
 
-        // Use this for initialization
+        /// <summary>
+        /// Use this for initialization
+        /// </summary>
         void Start()
         {
             this.IsGameOver = false;
         }
 
-        // Update is called once per frame
+        /// <summary>
+        /// Update is called once per frame
+        /// </summary>
         void Update()
         {
             if (this.IsGameOver)
@@ -27,12 +47,18 @@ namespace SparkAndLaser
             }
         }
 
+        /// <summary>
+        /// スタートボタンクリック
+        /// </summary>
         public void OnClickGameStartButton()
         {
-            Instantiate(this.StarFighter);
+            Instantiate(this.Fighter);
             this.GameStartButton.SetActive(false);
         }
 
+        /// <summary>
+        /// ゲームオーバーボタンクリック
+        /// </summary>
         public void OnClickGameOverButton()
         {
             Application.LoadLevel("MainScene");
