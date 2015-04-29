e51dfb2c224cf05b30080421a7b711b280bc11c9
tknemuru <kojikoji.max.5252@gmail.com>
2015-04-29 18:12:02 +0900

    camera control

--- Assets/MyAssets/Scripts/CameraControl.cs
+++ Assets/MyAssets/Scripts/CameraControl.cs
@@ -4,16 +4,25 @@ using System.Collections;
 public class CameraControl : MonoBehaviour {
 
     /// <summary>
+    /// 戦闘機との距離の差異
+    /// </summary>
+    private Vector3 Difference { get; set; }
+
+    /// <summary>
     /// Use this for initialization
     /// </summary>
 	void Start () {
-	
+        this.Difference = transform.localPosition;
 	}
 
     /// <summary>
     /// Update is called once per frame
     /// </summary>
 	void Update () {
-	
+        if (GameObject.Find("SciFi_Fighter_AK5"))
+        {
+            Vector3 startVector = GameObject.Find("SciFi_Fighter_AK5").transform.localPosition;
+            transform.localPosition = new Vector3(this.Difference.x, startVector.y + this.Difference.y, startVector.z + this.Difference.z);
+        }
 	}
 }
